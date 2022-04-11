6f3d40e6dbd5650f298173078fb1ae4fd7b83bc7
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-04-11 03:00:03 +0300

    html

--- api/date.py
+++ api/date.py
@@ -6,9 +6,46 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     #  request is successful and you will get your response
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/html')
     self.end_headers()
-    self.wfile.write(self.path[1:].encode())
+    output=''
+    output+="""<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>"""
+    output+="""<h1>Serverless Functions</h1>
+            <h2>Tasks</h2>
+            <table style="border-color: black;">
+                <tr>
+                <th>
+                    Task
+                </th>
+                
+                <th>progress</th>
+                
+                </tr>
+                <tr>
+                <td>Sign up with Vercel</td>
+                <td>Done</td>
+                </tr>
+                 <tr>
+                <td>Create a repository on Github and link it to Vercel account</td>
+                <td>Done</td>
+                </tr>
+                <tr>
+                <td>Create a simple html front end to interact with serverless function</td>
+                <td>Done</td>
+                </tr>
+            </table>  
+            </body>
+            </html>"""
+
+    self.wfile.write(output.encode())
     return
 
 # def main():
---
+++ api/howdy.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler , HTTPServer
+import requests
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    #  request is successful and you will get your response
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    r = requests.get('https://serverless-virid-eight.vercel.app/')
+    r.status_code
+    r.headers['content-type']
